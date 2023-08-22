c4f467a837b10ddbf84c481acd55a4c4c6c15c25
Vansh=Narang <vanshnarang49906@gmail.com>
2023-08-22 21:48:44 +0530

    Models created

---
+++ models/SubSection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const subSection = new Schema({
+
+    title: {
+        type: String,
+    },
+    timeDuration: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    videoUrl: {
+        type: String,
+    }
+})
+
+
+module.exports = mongoose.model("SubSection", subSection)

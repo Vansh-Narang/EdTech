f280c01566f44139f1969d8be1c71f5837251a53
Vansh=Narang <vanshnarang49906@gmail.com>
2023-08-23 19:29:46 +0530

    All models

--- models/Otp.js
+++ models/Otp.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const mailSender = require("../utils/mailSender")
 const OTPschema = new mongoose.Schema({
     email: {
         type: String,
@@ -14,6 +14,28 @@ const OTPschema = new mongoose.Schema({
         default: Date.now(),
         expires: 5 * 60
     }
+})
+//mail send for otp sending
+//user -> data ->mail aaya -> otp enter (submit)->db me entry create kro
+//mail send (pre)
+
+//a function ->to send mail
+async function sendVerificationMail(email, otp) {
+    try {
+        const mailResponse = await mailSender(email, "Verification Mail", otp)
+        console.log("mail send success", mailResponse)
+    } catch (error) {
+        console.log("Mail not send", error)
+        throw error
+    }
+}
+
+
+//pre
+OTPschema.pre("save", async function (next) {
+    //next pass krne se next work me chle jata hai
+    await sendVerificationMail(this.email, this.otp)
+    next()
 })
 
 module.exports = mongoose.model('OtpSchema', OTPschema);

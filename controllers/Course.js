769a8c5deb62f648526b95587f2e69b01fd7c755
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-02 23:01:41 +0530

    Created New course

--- controllers/Course.js
+++ controllers/Course.js
@@ -7,7 +7,77 @@ const { uploadImageToCloudinary } = require("../utils/imageUploader");
 //create and fetch
 exports.createCourse = async (req, res) => {
     try {
-        
+        //if logged in , or not logged in
+        //fetch the data
+
+        //accepting the tag as an id
+        const { courseName, courseDescription, whatYouWillLearn, price, tag } = req.body
+
+        //get thumbnail
+        const thumbnail = req.files.thumbnailImage;
+
+        //validation
+        if (!courseName || !courseDescription || !whatYouWillLearn || !price || !tag || !thumbnail) {
+            return res.status(400).json({
+                success: false,
+                message: "Please enter all fields"
+            })
+        }
+
+        //check for instructor to store in the id
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor Details: " + instructorDetails)
+
+        //if not data found
+        if (!instructorDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Instructors not found"
+            })
+        }
+
+        //check tags are valid
+        const tagDetails = await Tag.find(tag)
+        if (!tagDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Tags not found"
+            })
+        }
+
+        //upload to cloudinary (thumbnail)
+
+        const thumbnailImage = await uploadImageToCloudinary(thumbnail, "Codehelper")
+
+        //create an entry for new course
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id,
+            whatYouWillLearn,
+            price,
+            tag: tagDetails._id,
+            thumbnail: thumbnailImage.secure_url
+        })
+
+        //add the new course to user schema
+        await User.findByIdAndUpdate(
+            { _id: instructorDetails._id },
+            {
+                $push: {
+                    courses: newCourse._id,
+                }
+            },
+            { new: true }
+        )
+
+        //update the tag schema
+
+        return res.status(200).json({
+            message: true,
+            success: true,
+        })
     } catch (error) {
         res.status(500).json({
             success: false,

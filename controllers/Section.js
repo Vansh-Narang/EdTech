57d17bc207b288e7c4649091bf7c65dacc7c38e8
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-03 12:09:23 +0530

    Delete the id

--- controllers/Section.js
+++ controllers/Section.js
@@ -42,3 +42,64 @@ exports.createSection = async (req, res) => {
         })
     }
 }
+
+//update section
+exports.updateSection = async (req, res) => {
+    try {
+        //data input
+        const { sectionName, sectionId } = req.body;
+
+        //data validation
+        if (!sectionName || !sectionId) {
+            return res.status(400).json({
+                success: false,
+                message: "All section parameters are requiredS"
+            })
+        }
+        //data updated
+        const updatedSection = await Section.findByIdAndUpdate(sectionId,
+            { sectionName },
+            { new: true })
+
+        return res.status(200).json({
+            success: true,
+            message: "Section updated successfully",
+            updatedSection
+        })
+        //retun response
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Unable to update the section"
+        })
+    }
+}
+
+//delete section
+exports.deleteSection = async (req, res) => {
+    try {
+        //ask for the section id that we are asking the id from routes :id
+        const { sectionId } = req.params;
+
+        //find the section
+        if (!sectionId) {
+            return res.status(404).json({
+                success: false,
+                message: 'Cannot find the id',
+            })
+        }
+        //make the call to db to delete the section
+        await Section.findByIdAndDelete(sectionId);
+
+        res.status(200).json({
+            success: true,
+            message: "Section deleted successfully",
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Unable to delete the section"
+        })
+    }
+}

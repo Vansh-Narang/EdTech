f62a1a4859507cfc7e109f40d6c89338262f540d
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-04 22:01:10 +0530

    Complete Profile

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -33,12 +33,77 @@ exports.updateProfile = async (req, res) => {
         //return response
         res.status(200).json({
             success: true,
-            message: "Profile Updated Successfully"
+            message: "Profile Updated Successfully",
+            profileDetails
         })
     } catch (error) {
         res.status(404).json({
             success: false,
+            error: error,
             message: "Error updating profile"
+        })
+    }
+}
+
+//delete account
+exports.deleteAccount = async (req, res) => {
+    try {
+        //fetch data
+        const id = req.user.id
+
+        //validate profile
+        const userDetails = await User.findById(id)
+        if (!userDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "No profile for the id"
+            })
+        }
+        //find by id and delete
+        await Profile.findByIdAndDelete({ _id: userDetails.additionalDetails })
+        await User.findByIdAndDelete({ _id: id })
+        //json response
+
+
+        //to do [hw enroll when profile get deleted ]
+
+
+        res.status(200).json({
+            success: true,
+            message: "Deleted profile successfully"
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            error: error,
+            message: "Error deleting profile"
+        })
+    }
+}
+
+exports.getUserDetails = async (req, res) => {
+    try {
+        //fetch data
+        const id = req.user.id
+        //validate
+        if (!id) {
+            return res.status(404).json({
+                success: false,
+                message: "Id not found"
+            })
+        }
+        //find
+        const userDetails = await User.findById(id).populate("addittionalDetails").exec()
+        //return response
+        res.status(200).json({
+            success: true,
+            message: "Returned data"
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            error: error,
+            message: "Error deleting profile"
         })
     }
 }

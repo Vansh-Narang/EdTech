7f09051b6bd26013421ffa1b583eeb42a8d90606
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-05 23:24:33 +0530

    Payment Integration Completed

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -81,10 +81,114 @@ exports.capturePayment = async (req, res) => {
             console.log(error)
             res.json({
                 success: false,
-                message: "Couldnot processed the payment"
+                message: "Couldnot process the payment"
             })
         }
     } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Could not process the payment"
+        })
+    }
+}
+//verify the signature of razorpay and the backend
+exports.verifySignature = async (req, res) => {
+    //we have send a signature when the payment started in the encrypted formate and when the payment is done for authorzation we have to check in the end
+    try {
+        const webHookSecret = "12345678";
 
+        //syntax
+        const signature = req.headers("x-razorpay-signature")
+
+        //razorpay has send the signature in the hashed form so hashing must be done to 
+        //check
+
+        //hashed 
+
+        //digest is the ouptut of using the hashed algorithm
+        const shahsum = crypto.createHmac("sha256", webHookSecret)
+        shahsum.update(JSON.stringify(req.body))
+        //sha hashing is the hasing function
+        const digest = shahsum.digest("hex")
+        //hex is hexadecimal
+
+        //match signature with shahsum
+        if (signature === digest) {
+            console.log("payment is authorized")
+
+            //enroll in course
+
+            //map two methods
+            //1)--------User->map->course->objectID
+            //2)--------Course->studentsEnrolled->userID
+
+            //call is coming from the razorpay not from the front end so no data we can have in our body from req.body
+            //as we have send the data from the notes , so we can accept it from there the {course id and userid}
+
+            const { courseId, userId } = req.body.payload.payment.entity.notes;
+
+            try {
+                //fulfill the action
+                //find the course enroll the students in it
+                const enrollCourse = await Course.find({ _id: courseId },
+                    { $push: { studentsEnrolled: userId }, },
+                    { new: true })
+
+                if (!enrollCourse) {
+                    return res.status(500).json({
+                        success: false,
+                        message: "Could not enroll in the course"
+                    })
+                }
+                console.log(enrollCourse)
+
+                //find the students to course to thier list
+                const enrolledStudent = await User.findOneAndUpdate({ _id: userId },
+                    {
+                        $push: {
+                            courses: courseId
+                        }
+                    },
+                    { new: true })
+
+                if (!enrolledStudent) {
+                    return res.json({
+                        success: false,
+                        message: "Students cannot be updated"
+                    })
+                }
+                console.log(enrolledStudent)
+                //send email
+
+                //mail,title,body
+                const mailResponse = await mailSender(enrolledStudent.email, "Congratulations!", "Again congratulations")
+
+
+                console.log(mailResponse)
+                return res.status(200).json({
+                    success: true,
+                    message: "done with mail sending and enrollment"
+                })
+            } catch (error) {
+                console.log(error)
+                return res.status(500).json({
+                    success: false,
+                    message: error.message
+                })
+            }
+        }
+        else {
+            return res.status(400).json({
+                success: false,
+                message: "Couldnot match the signature"
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Could not verify the signature"
+        })
     }
 }

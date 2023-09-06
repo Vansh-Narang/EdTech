fe3e644fdb26ee03bea074b41cdfc10c43e45c27
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-06 22:58:49 +0530

    Category slightly remaining

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,6 +1,6 @@
 const Category = require("../models/Category")
 
-
+//create a category
 exports.createCategory = async (req, res) => {
     try {
         const { name, description } = req.body;
@@ -47,3 +47,43 @@ exports.showAllCategories = async (req, res) => {
     }
 }
 
+//category Page Details
+exports.categoryPageDetails = async (req, res) => {
+    try {
+        //category id is required
+        const { categoryId } = req.body
+        //get courses for specified category
+        const selectedCategory = await Category.findById(categoryId).populate("courses").exec()
+        //validation for courses in category
+        if (!selectedCategory) {
+            return res.status(400).json({
+                success: false,
+                message: "couldnot find courses for category"
+            })
+        }
+        //get courses for different categories
+        const differentCategories = await Category.find({
+            //aaise categories ka data lao jiski category iski category id ki equal nahi for recommended courses
+            //ne means not equal
+            _id: { $ne: categoryId },
+        }).populate("courses").exec()
+        //get top selling courses
+        //(konsa course kitne baar bik gya hai )
+
+
+        return res.status(200).json({
+            success: true,
+            data: {
+                selectedCategory,
+                differentCategories,
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            error: error,
+            message: "error in categoryPageDetails"
+        })
+    }
+}

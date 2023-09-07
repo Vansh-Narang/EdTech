60448ec8159166b1aac0ddf3f958ebbfcbb8dd8d
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-07 12:44:53 +0530

    Connected Port Testing left

--- routes/Course.js
+++ routes/Course.js
@@ -5,6 +5,51 @@ const { createCourse, showAllCourses, getCourseDetails } = require("../controlle
 const { showAllCategories, createCategory, categoryPageDetails } = require("../controllers/Category")
 const { createSection, updateSection, deleteSection } = require("../controllers/Section")
 const { createSubSection, updateSubSection, deleteSubSection } = require("../controllers/Subsection");
-const {createRating,getAverageRating,getAllRating,getAllReviewsForCourse } = require("../controllers/RatingAndReview")
+const { createRating, getAverageRating, getAllRating, getAllReviewsForCourse } = require("../controllers/RatingAndReview")
 
 //middlewares routes
+const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
+
+
+//courses will only be created by instructor
+router.post("/createCourse", auth, isInstructor, createCourse)
+
+//add a section to a course
+router.post("/addSection", auth, isInstructor, createSection)
+
+//update a section
+router.post("/updateSection", auth, isInstructor, updateSection)
+
+//delete a section
+router.post("/deleteSection", auth, isInstructor, deleteSection)
+
+//delete sub section
+router.post("/deleteSubSection", auth, isInstructor, deleteSubSection)
+
+//edit subsection
+router.post("editSubsection", auth, isInstructor, updateSubSection)
+
+//add a subsection to a section
+router.post("/addSubSection", auth, isInstructor, createSubSection)
+
+
+/****************************************************category**************************/
+router.get("/showAllCategory", showAllCategories);
+
+router.post("/createCategory", createCategory)
+
+router.post("/getCategoryPageDetails", categoryPageDetails)
+
+
+
+/*********************************************rating and reviews***************/
+router.post("/createRating", auth, isStudent, createRating)
+
+router.get("/getAverageRating", getAverageRating)
+
+router.get("/getReviews", getAllRating)
+
+
+module.exports = router
+
+
